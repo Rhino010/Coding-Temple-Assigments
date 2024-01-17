7b6a8a627831dde8727f8fbf34631a586989b636
Rhino010 <89215085+Rhino010@users.noreply.github.com>
2024-01-16 20:36:31 -0600

    Update prework.py

--- prework.py
+++ prework.py
@@ -1,18 +1,25 @@
 #Question 1
+user_name = "Ryan"
 
 def hello_name(user_name):
-    print(user_name)
+    print ('Hello ',user_name,'!')
+
+hello_name(user_name)
 
 
 #Question 2
 def first_odds():
-    for i in range(1,100,2)
-    print(i)
+    for i in range(1,100,2):
+        print(i)
 
 #Question 3
+a_list = [1,2,100,4,5]
+
 def max_num_in_list(a_list):
     a_list.sort()
     return(a_list[-1])
+    
+max_num_in_list(a_list)
 
 #Question 4
 def is_leap_year(year):
@@ -20,5 +27,9 @@ def is_leap_year(year):
     return (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0)
 
 #Question 5
+a_list = 1,2,4,5
+
 def is_consecutive(a_list):
     return sorted(a_list) == list(range(min(a_list),max(a_list)+1))
+
+is_consecutive(a_list)
